0e76372a951c81fee5a7096142da1bba416a3ef7
1hreeam <hypexund@gmail.com>
2026-08-13 01:18:42 +0200

    Add logging

--- generate_pdf.py
+++ generate_pdf.py
@@ -1,6 +1,10 @@
 from markdown_pdf import MarkdownPdf, Section
+import time as t
 
 def generate_pdf(markdown_text):
+    print("Generating PDF docunent...")
+    t.sleep(1)
+
     pdf = MarkdownPdf(optimize=True)
     pdf.add_section(Section("# Lorem Ipsum\n", toc=False))
     pdf.add_section(Section(markdown_text))
--- generate_text.py
+++ generate_text.py
@@ -1,19 +1,23 @@
 from random import randint
+import time as t
 
 def generate_random():
     lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
-    upperCaseLetters = lowerCaseLetters.upper()
     digits = "0123456789"
     specialCharacters = '.@;!()?"`:'
-    characters = lowerCaseLetters + upperCaseLetters + digits + specialCharacters
+    characters = lowerCaseLetters + lowerCaseLetters.upper() + digits + specialCharacters
 
     # length = input("Length: ")
     length = randint(1,2137)
     text = ""
 
+    print("Generating data...")
+    t.sleep(1)
+
     for i in range(int(length)):
         text = text + characters[randint(0, len(characters)-1)]
 
+    print("Data generated ✅")
     return text
 
 
